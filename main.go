409dded5a8431c123b8aa183c03d84328322b5ad
Ventsislav Kirkov <vince.kirkov@gmail.com>
2020-08-12 22:39:46 +0300

    https

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ import (
 
 func main() {
 
-	key := "Secret-session-key" // Replace with your SESSION_SECRET or similar
-	maxAge := 86400 * 30        // 30 days
-	isProd := false             // Set to true when serving over https
+	key := "co6fmd868j6eia81cto4145qt6bfe2ju" // Replace with your SESSION_SECRET or similar
+	maxAge := 86400 * 30                      // 30 days
+	isProd := true                            // Set to true when serving over https
 
 	store := sessions.NewCookieStore([]byte(key))
 	store.MaxAge(maxAge)
