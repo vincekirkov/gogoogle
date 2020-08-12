367e94f2f95f4df8530b391d2eb78465e2f4b8de
Ventsislav Kirkov <vince.kirkov@gmail.com>
2020-08-12 11:13:40 +0300

    test commit from code

--- main.go
+++ main.go
@@ -4,28 +4,26 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	
 
 	"log"
 
 	"github.com/gorilla/pat"
+	"github.com/gorilla/sessions"
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/google"
-	"github.com/gorilla/sessions"
 )
 
-
 func main() {
-	
-	key := "Secret-session-key"  // Replace with your SESSION_SECRET or similar
-	maxAge := 86400 * 30  // 30 days
-	isProd := false       // Set to true when serving over https
+
+	key := "Secret-session-key" // Replace with your SESSION_SECRET or similar
+	maxAge := 86400 * 30        // 30 days
+	isProd := false             // Set to true when serving over https
 
 	store := sessions.NewCookieStore([]byte(key))
 	store.MaxAge(maxAge)
 	store.Options.Path = "/"
-	store.Options.HttpOnly = true   // HttpOnly should always be enabled
+	store.Options.HttpOnly = true // HttpOnly should always be enabled
 	store.Options.Secure = isProd
 
 	gothic.Store = store
